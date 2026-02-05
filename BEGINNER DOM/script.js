ff42b98062930034fc7a7c086efba3b0bed88559
Fale1278 <kingnanmuafale@gmail.com>
2026-02-05 01:52:22 -0800

    Beginners Dom

---
+++ BEGINNER DOM/script.js
@@ -0,0 +1,22 @@
+
+
+// let para = document.getElementById("para");
+let paraTwo = document.querySelector(".paraTwo");
+
+// para.textContent  = "Changing text content."
+// paraTwo.textContent = "Also changing the second paragraph."
+
+
+// para.style.backgroundColor = "red";
+// paraTwo.style.color = "blue";
+
+// para.style.fontSize = "30px";
+
+
+// let p = document.createElement("p");
+// p.textContent = "I am a new paragraph.";
+// document.body.appendChild(p);
+
+// paraTwo.remove()
+
+// Events - Basically what users do.
