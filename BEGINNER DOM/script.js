5ef395e59faa47ccbc4661f83b9ebe910ada4322
Fale1278 <kingnanmuafale@gmail.com>
2026-02-06 02:57:21 -0800

    Intermediate dom

--- BEGINNER DOM/script.js
+++ BEGINNER DOM/script.js
@@ -34,12 +34,77 @@
 //     paraTwo.textContent = "Button Clicked..."
 // })
 
-let count  = 0;
+// let count  = 0;
 
-let counter = document.getElementById("counter");
-let addBtn = document.getElementById("addBtn");
+// let counter = document.getElementById("counter");
+// let addBtn = document.getElementById("addBtn");
+// let subBtn = document.getElementById("subBtn");
 
-addBtn.addEventListener("click", function(){
-    count++;
-    counter.textContent = count;
-})
+// addBtn.addEventListener("click", function(){
+//     count++;
+//     counter.textContent = count;
+// })
+
+// subBtn.addEventListener("click", function(){
+//     count--;
+//     counter.textContent = count;
+// })
+
+// let title = document.getElementById("title");
+// let btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function(){
+//     title.style.color = "red";
+// })
+
+// let btn = document.getElementById("btn")
+
+// btn.addEventListener("click", function(){
+//     document.body.style.backgroundColor = "red"
+// })
+
+// let info = document.getElementById("info");
+// let toggle = document.getElementById("toggle");
+
+// toggle.addEventListener("click", function(){
+//     if(info.style.display === "none"){
+//         info.style.display = "block";
+
+//     }
+
+//     else{
+//         info.style.display = "none";
+//     }
+// })
+
+// let list = document.getElementById("list");
+// let addBtn = document.getElementById("add");
+
+// addBtn.addEventListener("click", function(){
+//     let li = document.createElement("li");
+//     li.textContent = "New Item";
+//     list.appendChild(li);
+// });
+
+
+let login = document.getElementById("login");
+let message = document.getElementById("message");
+
+login.addEventListener("click",function(){
+    
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    
+    if(username === "admin" && password === "123456"){
+        message.textContent = "Login Successful...";
+        message.style.color = "green"
+        message.style.fontStyle = "italic"
+    }
+
+    else{
+        message.textContent = "Wrong Details...";
+        message.style.color = "red"
+        message.style.fontStyle = "italic"
+    }
+});
